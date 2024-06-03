df3afe4cc4a391c2c26c2736b792619ec43e1515
Mohammed Thasleem MK <mohammedthasleemmk6604@gmail.com>
2024-06-03 09:41:54 +0530

    Updated

--- Index.js
+++ Index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-// const mongoose = require("mongoose");
 const usersRoute = require("./Routes/User");
 const adminRoute = require("./Routes/Admin");
 const dbConnect = require("./config/dbConnection");
