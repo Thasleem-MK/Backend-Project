eafa3dae0a268e9a5a71645c68ad8d235b5c6155
Mohammed Thasleem MK <mohammedthasleemmk6604@gmail.com>
2024-06-04 12:19:03 +0530

    updated

--- Controller/UserController.js
+++ Controller/UserController.js
@@ -10,6 +10,7 @@ const stripeID = require('stripe')("sk_test_51P5UXZSBDzdy1QTWpNIKtArSEQbiMZjfYDY
 //............... User Profile .....................
 const userProfile = async (req, res) => {
   const { token } = req.cookies;
+  console.log(token);
   const decode = await jwt.verify(token, process.env.SecretKey);
   console.log(decode);
   const user = await userSchema.findById(decode.email);
